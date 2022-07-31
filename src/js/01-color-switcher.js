cbcdbd6ad9b674b452323b98db24983bc67ff938
Lora <lorat1403@gmail.com>
2022-07-31 18:16:53 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyRef = document.body;
+let onChangeColor;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+startBtn.addEventListener('click', () => {
+  onChangeColor = setInterval(() => {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(onChangeColor);
+  startBtn.disabled = false;
+});
